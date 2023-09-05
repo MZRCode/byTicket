4cdb4a094447df7ab4898cb5425e53a796a1db7b
mzrdev <87870017+MZRCode@users.noreply.github.com>
2023-09-05 13:42:43 +0300

    Update SlashCommands.js

--- Events/Client/Interactions/SlashCommands.js
+++ Events/Client/Interactions/SlashCommands.js
@@ -23,7 +23,7 @@ module.exports = {
         if(subCommand) {
             const subCommandFile = client.subCommands.get(`${interaction.commandName}.${subCommand}`);
             if(!subCommandFile) return interaction.reply({ content: "Bu komut artık kullanılmıyor!", ephemeral: true });
-            subCommandFile.execute(interaction, client);
-        } else command.execute(interaction, client);
+            subCommandFile.execute();
+        } else command.execute();
     }
 }
